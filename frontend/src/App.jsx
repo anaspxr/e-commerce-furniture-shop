b75137492c78acf01285f924817653524bbf8716
ANAS <anaspappadan@gmail.com>
2024-06-22 12:07:27 +0530

    Create page components and setup routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,31 @@
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import LoginSignup from "./pages/LoginSignup";
+import Categories from "./pages/Categories";
+import Product from "./pages/Product";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/furniture"
+            element={<Categories category="furniture" />}
+          />
+          <Route path="/sofas" element={<Categories category="sofas" />} />
+          <Route path="/dining" element={<Categories category="dining" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productID" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
