fe7d088f9dd9df787d5d4d017b95fa6e35bfbd2c
ANAS <anaspappadan@gmail.com>
2024-06-25 13:31:30 +0530

    update furniture data with more items

--- frontend/src/components/assets/data.js
+++ frontend/src/components/assets/data.js
@@ -11,7 +11,7 @@ export const furnitureData = [
     image: sofa,
     description:
       "A stylish and comfortable modern sofa perfect for any living room.",
-    category: "sofa",
+    category: "sofas",
   },
   {
     id: 2,
@@ -21,7 +21,7 @@ export const furnitureData = [
     image: sofa,
     description:
       "An elegant classic armchair that adds a touch of sophistication to your space.",
-    category: "sofa",
+    category: "furnishings",
   },
   {
     id: 3,
@@ -31,7 +31,7 @@ export const furnitureData = [
     image: dining,
     description:
       "A large wooden dining table that brings a rustic charm to your dining area.",
-    category: "kitchen and dining",
+    category: "dining",
   },
   {
     id: 4,
@@ -41,7 +41,7 @@ export const furnitureData = [
     image: decor,
     description:
       "A comfortable ergonomic office chair designed to support your back during long hours of work.",
-    category: "home office",
+    category: "furnishings",
   },
   {
     id: 5,
@@ -51,7 +51,7 @@ export const furnitureData = [
     image: decor,
     description:
       "A sleek and minimalist coffee table that complements modern interiors.",
-    category: "living room",
+    category: "homedecor",
   },
   {
     id: 6,
@@ -61,7 +61,7 @@ export const furnitureData = [
     image: sofa,
     description:
       "A luxurious bed frame that provides both comfort and style to your bedroom.",
-    category: "bedroom",
+    category: "matresses",
   },
   {
     id: 7,
@@ -71,7 +71,7 @@ export const furnitureData = [
     image: decor,
     description:
       "A compact and sturdy bookshelf ideal for small spaces and book lovers.",
-    category: "home office",
+    category: "furnishings",
   },
   {
     id: 8,
@@ -81,7 +81,7 @@ export const furnitureData = [
     image: decor,
     description:
       "A durable outdoor patio set perfect for enjoying sunny days and outdoor gatherings.",
-    category: "outdoor",
+    category: "furnishings",
   },
   {
     id: 9,
@@ -91,7 +91,7 @@ export const furnitureData = [
     image: dining,
     description:
       "A fun and functional bunk bed designed for kids' bedrooms with safety features.",
-    category: "kids",
+    category: "matresses",
   },
   {
     id: 10,
@@ -101,7 +101,7 @@ export const furnitureData = [
     image: sofa,
     description:
       "A plush recliner sofa that offers ultimate comfort and relaxation with adjustable settings.",
-    category: "sofa",
+    category: "sofas",
   },
   {
     id: 11,
@@ -111,7 +111,7 @@ export const furnitureData = [
     image: decor,
     description:
       "A beautifully crafted decorative vase that adds elegance to any room.",
-    category: "home decor",
+    category: "homedecor",
   },
   {
     id: 12,
@@ -121,6 +121,185 @@ export const furnitureData = [
     image: dining,
     description:
       "A set of four comfortable dining chairs with a modern design.",
-    category: "kitchen and dining",
+    category: "dining",
+  },
+  {
+    id: 13,
+    name: "Rustic TV Stand",
+    price: 8999,
+    discount: 8,
+    image: decor,
+    description:
+      "A rustic TV stand with ample storage space and a charming design.",
+    category: "furnishings",
+  },
+  {
+    id: 14,
+    name: "Elegant Floor Lamp",
+    price: 2999,
+    discount: 12,
+    image: decor,
+    description: "An elegant floor lamp that adds a warm glow to any room.",
+    category: "lightings",
+  },
+  {
+    id: 15,
+    name: "Modern Wall Art",
+    price: 1599,
+    discount: 7,
+    image: decor,
+    description:
+      "A piece of modern wall art that brings creativity and style to your walls.",
+    category: "homedecor",
+  },
+  {
+    id: 16,
+    name: "Luxury Recliner Chair",
+    price: 17999,
+    discount: 18,
+    image: sofa,
+    description:
+      "A luxurious recliner chair that offers exceptional comfort and style.",
+    category: "sofas",
+  },
+  {
+    id: 17,
+    name: "Glass Coffee Table",
+    price: 5999,
+    discount: 10,
+    image: decor,
+    description:
+      "A sleek glass coffee table that adds a modern touch to your living room.",
+    category: "homedecor",
+  },
+  {
+    id: 18,
+    name: "Queen Size Mattress",
+    price: 20999,
+    discount: 20,
+    image: sofa,
+    description:
+      "A queen size mattress that provides superior comfort and support for a good night's sleep.",
+    category: "matresses",
+  },
+  {
+    id: 19,
+    name: "Leather Office Chair",
+    price: 7999,
+    discount: 15,
+    image: decor,
+    description:
+      "A stylish leather office chair designed for maximum comfort during long work hours.",
+    category: "furnishings",
+  },
+  {
+    id: 20,
+    name: "Dining Bench",
+    price: 7999,
+    discount: 12,
+    image: dining,
+    description:
+      "A sturdy dining bench that offers additional seating for your dining area.",
+    category: "dining",
+  },
+  {
+    id: 21,
+    name: "Artistic Table Lamp",
+    price: 2499,
+    discount: 10,
+    image: decor,
+    description:
+      "An artistic table lamp that adds a unique touch to your workspace or bedside table.",
+    category: "lightings",
+  },
+  {
+    id: 22,
+    name: "Velvet Ottoman",
+    price: 3499,
+    discount: 15,
+    image: decor,
+    description:
+      "A plush velvet ottoman that provides extra seating and elegance to your living room.",
+    category: "furnishings",
+  },
+  {
+    id: 23,
+    name: "Bamboo Shelf",
+    price: 6999,
+    discount: 20,
+    image: decor,
+    description:
+      "A sustainable bamboo shelf that offers both storage and a natural aesthetic.",
+    category: "homedecor",
+  },
+  {
+    id: 24,
+    name: "Convertible Sofa Bed",
+    price: 22999,
+    discount: 15,
+    image: sofa,
+    description:
+      "A versatile convertible sofa bed perfect for guests and small spaces.",
+    category: "sofas",
+  },
+  {
+    id: 25,
+    name: "Crystal Chandelier",
+    price: 15999,
+    discount: 25,
+    image: decor,
+    description:
+      "A luxurious crystal chandelier that adds a touch of elegance to any room.",
+    category: "lightings",
+  },
+  {
+    id: 26,
+    name: "Children's Toy Storage",
+    price: 4999,
+    discount: 10,
+    image: decor,
+    description:
+      "A practical and fun toy storage unit for organizing children's play areas.",
+    category: "furnishings",
+  },
+  {
+    id: 27,
+    name: "Vintage Bookshelf",
+    price: 9999,
+    discount: 12,
+    image: decor,
+    description:
+      "A vintage bookshelf that brings a classic look to your home library or living room.",
+    category: "furnishings",
+  },
+  {
+    id: 28,
+    name: "Modern Pendant Light",
+    price: 3499,
+    discount: 10,
+    image: decor,
+    description:
+      "A modern pendant light that enhances the style of your kitchen or dining area.",
+    category: "lightings",
+  },
+  {
+    id: 29,
+    name: "Adjustable Desk Lamp",
+    price: 1999,
+    discount: 5,
+    image: decor,
+    description:
+      "A sleek and adjustable desk lamp perfect for studying or working late hours.",
+    category: "lightings",
+  },
+  {
+    id: 30,
+    name: "Comfortable Rocking Chair",
+    price: 11999,
+    discount: 18,
+    image: decor,
+    description:
+      "A comfortable rocking chair that provides relaxation and a timeless look.",
+    category: "furnishings",
   },
 ];
