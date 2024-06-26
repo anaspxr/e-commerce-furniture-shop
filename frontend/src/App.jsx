25fd875e6c03145988c092efb27465cb4f33a867
ANAS <anaspappadan@gmail.com>
2024-06-26 12:03:12 +0530

    create cartContext

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,46 +9,49 @@ import Profile from "./pages/Profile";
 import { UserProvider } from "./contexts/UserContext";
 import Product from "./pages/Product";
 import ScrollToTop from "./components/ScrollToTop";
+import { CartContextProvider } from "./contexts/CartContext";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       <UserProvider>
-        <BrowserRouter>
-          <Navbar />
-          <div className="md:pt-24 pt-16 "></div>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/products">
-              <Route index element={<Products category="furniture" />} />
-              <Route
-                path="homedecor"
-                element={<Products category="homedecor" />}
-              />
-              <Route path="sofas" element={<Products category="sofas" />} />
-              <Route
-                path="mattresses"
-                element={<Products category="mattresses" />}
-              />
-              <Route path="dining" element={<Products category="dining" />} />
-              <Route
-                path="lightings"
-                element={<Products category="lightings" />}
-              />
-              <Route
-                path="furnishings"
-                element={<Products category="furnishings" />}
-              />
-              <Route path=":productID" element={<Product />} />
-            </Route>
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<LoginSignup />} />
-            <Route path="/profile" element={<Profile />} />
-          </Routes>
+        <CartContextProvider>
+          <BrowserRouter>
+            <Navbar />
+            <div className="md:pt-24 pt-16 "></div>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products">
+                <Route index element={<Products category="furniture" />} />
+                <Route
+                  path="homedecor"
+                  element={<Products category="homedecor" />}
+                />
+                <Route path="sofas" element={<Products category="sofas" />} />
+                <Route
+                  path="mattresses"
+                  element={<Products category="mattresses" />}
+                />
+                <Route path="dining" element={<Products category="dining" />} />
+                <Route
+                  path="lightings"
+                  element={<Products category="lightings" />}
+                />
+                <Route
+                  path="furnishings"
+                  element={<Products category="furnishings" />}
+                />
+                <Route path=":productID" element={<Product />} />
+              </Route>
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/login" element={<LoginSignup />} />
+              <Route path="/profile" element={<Profile />} />
+            </Routes>
 
-          <Footer />
-          <ScrollToTop />
-        </BrowserRouter>
+            <Footer />
+            <ScrollToTop />
+          </BrowserRouter>
+        </CartContextProvider>
       </UserProvider>
     </div>
   );
---
+++ frontend/src/contexts/CartContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState } from "react";
+
+export const CartContext = createContext();
+
+export function CartContextProvider({ children }) {
+  const [cartItems, setCartItems] = useState({});
+  function addToCart(productID) {
+    setCartItems((prev) => {
+      console.log(prev);
+      if (prev[productID]) {
+        return { ...prev, [productID]: prev[productID] + 1 };
+      }
+      return { ...prev, [productID]: 1 };
+    });
+  }
+
+  function removeFromCart(productID) {
+    setCartItems((prev) => {
+      console.log(prev);
+      if (prev[productID] === 1) {
+        const newCartItems = { ...prev };
+        delete newCartItems[productID];
+        return newCartItems;
+      }
+      return { ...prev, [productID]: prev[productID] - 1 };
+    });
+  }
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
