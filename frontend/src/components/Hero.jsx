e57ab21b653fc6daf40b1eda24d8cee8b8d080c3
ANAS <anaspappadan@gmail.com>
2024-06-27 10:29:03 +0530

    update font style and hero content

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,12 +6,22 @@ export default function Hero() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 justify-around items-center p-5 gap-2 h-svh">
       <div>
-        <h1 className="md:text-6xl text-4xl text-orange-800">
-          Welcome To Furnify..!!
-        </h1>
+        <h1 className="md:text-6xl text-4xl text-orange-800">Furnify</h1>
+        <p className="text-lg text-orange-900">
+          One stop solution for all your furniture needs
+        </p>
+        <p className="text-orange-900">
+          Discover Elegant and affordable furniture for every room in your
+          house.
+        </p>
         <Link to="/products">
           <div className=" mt-10">
-            <Button>Shop Now..!!</Button>
+            <Button>Shop Now</Button>
+          </div>
+        </Link>
+        <Link to="/products">
+          <div className=" mt-10">
+            <Button>Browse Categories</Button>
           </div>
         </Link>
       </div>
