8d567a92b9f86428efec393a1692c762a0dcb5bc
ANAS <anaspappadan@gmail.com>
2024-06-27 10:23:02 +0530

    update footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,20 +1,85 @@
 import { Link } from "react-router-dom";
 import logo from "./assets/logo.png";
+import {
+  FaSquareFacebook,
+  FaSquareInstagram,
+  FaSquareXTwitter,
+  FaSquareGithub,
+} from "react-icons/fa6";
+
 export default function Footer() {
   return (
     <div className="mt-auto">
-      <footer className="bg-orange-100 text-orange-800 p-3 text-center">
-        <div className="flex items-center flex-col">
-          <Link to="/">
-            <img className="max-w-36" src={logo} alt="Furnify" />
-          </Link>
-          <p>One stop solution for all your furniture needs</p>
-        </div>
-        <div>
-          <h2 className="text-2xl">Contact Us</h2>
-          <p>Address: 1234, Furnify Street, Kerala, India</p>
-          <p>Email: furnify@mail.com | Phone: 1234567890</p>
-          <p>&copy; 2021 Furnify</p>
+      <footer className="bg-orange-100 text-orange-800 p-3 py-10 font-serif">
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+          <div className="flex items-start flex-col">
+            <Link to="/">
+              <img className="max-w-36" src={logo} alt="Furnify" />
+            </Link>
+            <p>One stop solution for all your furniture needs</p>
+            <p>Email: furnify@mail.com </p>
+            <p>&copy; 2021 Furnify</p>
+          </div>
+
+          <div className="mt-4">
+            <h2 className="text-xl text-orange-900">Company</h2>
+            <ul>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact Us</Link>
+              </li>
+              <li>
+                <Link to="/products">Terms and Conditions</Link>
+              </li>
+              <li>
+                <Link to="/cart">Privacy Policy</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="mt-4">
+            <h2 className="text-xl text-orange-900">Quick Links</h2>
+            <ul>
+              <li>
+                <Link to="/">Scroll to Top </Link>
+              </li>
+              <li>
+                <Link to="/produts">Categories</Link>
+              </li>
+              <li>
+                <Link to="/products">Products</Link>
+              </li>
+              <li>
+                <Link to="/cart">Cart</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="mt-4">
+            <h2 className="text-xl text-orange-900">Connect With Us</h2>
+            <ul className="flex gap-3 text-2xl">
+              <li className="hover:text-orange-700">
+                <a href="https://www.facebook.com">
+                  <FaSquareFacebook />
+                </a>
+              </li>
+              <li className="hover:text-orange-700">
+                <a href="https://www.twitter.com">
+                  <FaSquareXTwitter />
+                </a>
+              </li>
+              <li className="hover:text-orange-700">
+                <a href="https://www.instagram.com">
+                  <FaSquareInstagram />
+                </a>
+              </li>
+              <li className="hover:text-orange-700">
+                <a href="https://www.github.com">
+                  <FaSquareGithub />
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </footer>
     </div>
