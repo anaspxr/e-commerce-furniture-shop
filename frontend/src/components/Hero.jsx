ece105d07218b6fca292d0d36a26b3cc56b64c51
ANAS <anaspappadan@gmail.com>
2024-06-30 13:53:45 +0530

    fix responsiveness issue on hero image

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -6,13 +6,13 @@ export default function Hero() {
   return (
     <div className="grid md:grid-cols-2 items-center p-5 gap-2 md:px-20 2xl:px-52">
       <div className="md:-mt-10">
-        <h1 className="md:text-6xl text-4xl xl:text-8xl 2xl:text-9xl text-orange-800 my-1 font-bold">
+        <h1 className="md:text-6xl text-5xl xl:text-8xl 2xl:text-9xl text-orange-900 my-1 font-bold">
           Comfort Craft
         </h1>
-        <p className="text-lg text-orange-900">
+        <p className="text-lg text-orange-800">
           One stop solution for all your furniture needs
         </p>
-        <p className="text-orange-900">
+        <p className="text-orange-800">
           Discover Elegant and affordable furniture for every room in your
           house.
         </p>
@@ -26,7 +26,7 @@ export default function Hero() {
         </div>
       </div>
       <div className="flex justify-center">
-        <img className="max-w-md md:max-w-full " src={heroImage} alt="" />
+        <img className="md:max-w-full sm:max-w-lg" src={heroImage} alt="" />
       </div>
     </div>
   );
