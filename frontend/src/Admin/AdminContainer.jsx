1982033c7afe2fddd87bc3cbbaab49aae49ec000
ANAS <anaspappadan@gmail.com>
2024-07-02 17:15:30 +0530

    add filter functionality to admin products page

--- frontend/src/Admin/AdminContainer.jsx
+++ frontend/src/Admin/AdminContainer.jsx
@@ -23,34 +23,37 @@ export default function Admin({ children }) {
   const { logout } = useContext(UserContext);
   return (
     <>
-      <div className="flex items-center bg-orange-100 justify-between fixed top-0 z-50 w-full  border-b border-orange-200 px-3 py-3 lg:px-5 lg:pl-3 shadow-md">
+      <div className="flex items-center bg-slate-100 justify-between fixed top-0 z-50 w-full  border-b border-slate-200 px-3 py-3 lg:px-5 lg:pl-3 shadow-md">
         <div className="flex items-center justify-start rtl:justify-end">
           <button
             onClick={() => {
               setIsOpen(!isOpen);
             }}
             type="button"
-            className="inline-flex items-center p-2 text-sm text-orange-950 hover:text-orange-800 rounded-lg sm:hidden hover:bg-orange-100 focus:outline-none focus:ring-2 focus:ring-orange-200 "
+            className="inline-flex items-center p-2 text-sm text-slate-950 hover:text-slate-800 rounded-lg sm:hidden hover:bg-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-200 "
           >
             <RiMenu2Line className="text-3xl" />
           </button>
           <Link to="/" className="flex ms-2 md:me-24">
             <img src={icon} className="h-8 me-3" alt="FlowBite Logo" />
             <span className="text-orange-950 self-center text-xl font-semibold sm:text-2xl whitespace-nowrap ">
-              Admin
+              Comfort Craft
             </span>
           </Link>
         </div>
         <div className="flex items-center ms-3">
-          <MdAdminPanelSettings className="text-4xl text-orange-900" />
+          <MdAdminPanelSettings className="text-4xl text-slate-900" />
         </div>
       </div>
       <div
         className={`fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform ${
           !isOpen && "-translate-x-full"
-        }  bg-orange-100 border-r border-orange-200 sm:translate-x-0`}
+        }  bg-slate-100 border-r border-slate-200 sm:translate-x-0`}
       >
         <div className="h-full px-3 overflow-y-auto ">
+          <p className="text-slate-900 text-center text-xl font-semibold sm:text-2xl border-b py-2 mb-5">
+            Admin
+          </p>
           <ul className="space-y-2 font-medium">
             {navItems.map((item, index) => (
               <li key={index}>
@@ -58,7 +61,7 @@ export default function Admin({ children }) {
                   to={item.link}
                   className={`${
                     location === item.link && "bg-white"
-                  } flex items-center p-2 text-orange-950 rounded-lg  hover:bg-orange-200  group`}
+                  } flex items-center p-2 text-slate-950 rounded-lg  hover:bg-slate-200  group`}
                 >
                   {item.icon}
                   <span className="flex-1 ms-3 whitespace-nowrap">
@@ -68,7 +71,7 @@ export default function Admin({ children }) {
               </li>
             ))}
             <li
-              className="cursor-pointer flex items-center p-2 text-orange-950 rounded-lg  hover:bg-orange-200  group"
+              className="cursor-pointer flex items-center p-2 text-slate-950 rounded-lg  hover:bg-slate-200  group"
               onClick={logout}
             >
               <PiSignOutFill />
