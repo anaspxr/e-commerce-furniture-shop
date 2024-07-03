f8d39dc88791f11f0ef9749a76ef70e16ab216ca
ANAS <anaspappadan@gmail.com>
2024-07-03 11:50:54 +0530

    add more user data and update userDetails section

--- frontend/src/Admin/UsersPage.jsx
+++ frontend/src/Admin/UsersPage.jsx
@@ -4,10 +4,18 @@ import useFetch from "../utils/useFetch";
 export default function UsersPage() {
   const {
     data: users,
-    loading,
-    error,
+    loading: loadingUsers,
+    error: errorUsers,
   } = useFetch("http://localhost:3000/users");
+
+  const {
+    data: products,
+    loading: loadingProducts,
+    error: errorProducts,
+  } = useFetch("http://localhost:3000/products");
+
   const [selectedUser, setSelectedUser] = useState(null);
+
   return (
     <div>
       <h1 className="text-2xl font-semibold">Users</h1>
@@ -15,44 +23,76 @@ export default function UsersPage() {
         <div className="bg-slate-200 p-3 rounded-md text-slate-600">
           <h2 className="text-xl  font-semibold">{selectedUser.name}</h2>
           <p>{selectedUser.email}</p>
-          <p>Orders: {JSON.stringify(selectedUser.orders, null, 2)}</p>
-          <p>In cart: {JSON.stringify(selectedUser.cart)}</p>
+          {products ? (
+            <div>
+              <p className="font-bold">Orders:</p>
+              {Object.keys(selectedUser.orders).map((productID) => {
+                const product = products.find((item) => item.id === productID);
+
+                return (
+                  <p key={productID}>
+                    {product.name} x {selectedUser.orders[productID]}{" "}
+                  </p>
+                );
+              })}
+            </div>
+          ) : (
+            <>
+              {loadingProducts && <p>Loading</p>}
+              {errorProducts && (
+                <p>Error fetching products data {errorProducts} </p>
+              )}
+            </>
+          )}
         </div>
       )}
-      {error && <p>{error.message}</p>}
-      {loading && <p>Loading...</p>}
+      {errorUsers && <p>{errorUsers.message}</p>}
+      {loadingUsers && <p>Loading...</p>}
       {users && <UsersList users={users} setSelectedUser={setSelectedUser} />}
     </div>
   );
 }
 
 function UsersList({ users, setSelectedUser }) {
+  const [toggle, setToggle] = useState(false);
   return (
     <div>
-      <ul className="flex flex-col justify-between space-y-2 mt-4">
-        {users.map((user) => (
-          <li
-            key={user.id}
-            className="flex justify-between sm p-2 bg-gray-100 rounded-md"
-          >
-            <div>
-              <p className="text-slate-500">{user.name}</p>
-              <p className="text-slate-700">{user.email}</p>
-              <p className="text-slate-600">
-                Orders:{Object.keys(user.orders).length}
-              </p>
-            </div>
-            <button
-              className="text-blue-500 w-40 p-1 rounded-md hover:bg-slate-200"
-              onClick={() => {
-                window.scrollTo(0, 0);
-                setSelectedUser(user);
-              }}
+      <button
+        className="bg-slate-500 text-white rounded-md p-1 text-sm my-3 hover:bg-opacity-90"
+        onClick={() => setToggle(!toggle)}
+      >
+        {toggle ? "Show Users" : "Show Admins"}
+      </button>
+      <ul className="flex flex-col justify-between space-y-2">
+        {users
+          .filter((user) => (toggle ? user.isAdmin : !user.isAdmin))
+          .map((user) => (
+            <li
+              key={user.id}
+              className="flex justify-between sm p-2 bg-gray-100 rounded-md"
             >
-              See Details
-            </button>
-          </li>
-        ))}
+              <div>
+                <p className="text-slate-500">{user.name}</p>
+                <p className="text-slate-700">{user.email}</p>
+                {!toggle && (
+                  <p className="text-slate-600">
+                    Orders:{Object.keys(user.orders).length}
+                  </p>
+                )}
+              </div>
+              {!toggle && (
+                <button
+                  className="text-blue-500 w-40 p-1 rounded-md hover:bg-slate-200"
+                  onClick={() => {
+                    window.scrollTo(0, 0);
+                    setSelectedUser(user);
+                  }}
+                >
+                  See Details
+                </button>
+              )}
+            </li>
+          ))}
       </ul>
     </div>
   );
