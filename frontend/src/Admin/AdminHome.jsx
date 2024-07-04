5b41bd4062a7635e0eca887b90caa4fda95c087e
ANAS <anaspappadan@gmail.com>
2024-07-04 16:25:08 +0530

    refactor: some design updates on admin navbar

--- frontend/src/Admin/AdminHome.jsx
+++ frontend/src/Admin/AdminHome.jsx
@@ -13,12 +13,16 @@ export default function Admin() {
     error: errorProducts,
   } = useFetch("http://localhost:3000/products");
 
+  const totalOrders =
+    users &&
+    users.reduce((total, user) => total + Object.keys(user.orders).length, 0);
+
   return (
     <div>
       <div className="grid  md:grid-cols-2 gap-2">
         <Link
           to="/admin/users"
-          className="bg-slate-300 px-2 py-5 rounded-md text-slate-700 hover:bg-opacity-80"
+          className="bg-slate-200 px-2 py-5 rounded-md text-slate-500 hover:bg-slate-300"
         >
           <p className="text-3xl mb-5">Users</p>
           {loadingUsers && <p>Loading...</p>}
@@ -27,22 +31,19 @@ export default function Admin() {
         </Link>
         <Link
           to="/admin/products"
-          className="bg-slate-300 px-2 py-5 rounded-md text-slate-700 hover:bg-opacity-80"
+          className="bg-slate-200 px-2 py-5 rounded-md text-slate-500 hover:bg-slate-300"
         >
           <p className="text-3xl mb-5">Products</p>
           {loadingProducts && <p>Loading...</p>}
           {errorProducts && <p>Error: {errorProducts.message}</p>}
           <p className="text-2xl">{products && products.length}</p>
         </Link>
-        <Link
-          to="/admin/products"
-          className="bg-slate-300 px-2 py-5 rounded-md text-slate-700 hover:bg-opacity-80"
-        >
-          <p className="text-3xl mb-5">Orders</p>
+        <div className="bg-slate-200 px-2 py-5 rounded-md text-slate-500">
+          <p className="text-3xl mb-5">Total Orders</p>
           {loadingProducts && <p>Loading...</p>}
           {errorProducts && <p>Error: {errorProducts.message}</p>}
-          <p className="text-2xl">{products && 5}</p>
-        </Link>
+          <p className="text-2xl">{products && totalOrders}</p>
+        </div>
       </div>
     </div>
   );
