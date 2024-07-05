967d8121caa0a23730dad5b1f90b995ffae034ef
ANAS <anaspappadan@gmail.com>
2024-07-05 11:10:00 +0530

    feat: add product data from server to ui - popular products

--- frontend/src/components/Popular.jsx
+++ frontend/src/components/Popular.jsx
@@ -1,26 +1,33 @@
-import { furnitureData } from "./assets/data";
+import { useContext } from "react";
 import Item from "./Item";
-
-const popularProducts = [
-  furnitureData[0], // Modern Sofa
-  furnitureData[2], // Wooden Dining Table
-  furnitureData[4], // Minimalist Coffee Table
-  furnitureData[5], // Luxury Bed Frame
-  furnitureData[6], // Compact Bookshelf
-  furnitureData[9], // Recliner Sofa
-];
+import { ProductContext } from "../contexts/ProductContext";
 
 export default function Popular() {
+  const { products, loading, error } = useContext(ProductContext) || [];
+  const popularProducts = products
+    ? [
+        products[0],
+        products[2],
+        products[4],
+        products[5],
+        products[6],
+        products[9],
+      ]
+    : null;
   return (
     <div className="p-2 sm:p-3 lg:p-5 pb-10 bg-amber-100">
       <h2 className="md:text-4xl text-3xl text-orange-900 py-5 text-center">
         Popular Products
       </h2>
-      <div className="grid grid-cols-2 md:grid-cols-3 gap-2 sm:gap-4">
-        {popularProducts.map((item) => (
-          <Item key={item.id} product={item} />
-        ))}
-      </div>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {popularProducts && (
+        <div className="grid grid-cols-2 md:grid-cols-3 gap-2 sm:gap-4">
+          {popularProducts.map((item) => (
+            <Item key={item.id} product={item} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
